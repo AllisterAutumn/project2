8549e576f24a4a80dbf1773e4afc4c0b311f9b94
val rigodon <rvrigodon@gmail.com>
2017-01-17 21:46:33 -0500

    this commit adds styling

--- project2/src/ChapterBox.js
+++ project2/src/ChapterBox.js
@@ -3,10 +3,8 @@ import axios from 'axios';
 
 class ChapterBox extends Component {
 
-
   handleEdit() {
-    console.log(this.props.chapterId, this.refs.edit.value)
-    //axios.put(`https://project2-a12a5.firebaseio.com/story/${this.props.chapterId}/title/.json`, [this.refs.edit.value]);
+    console.log(this.props.chapterId, this.refs.edit.value);
     this.props.editChapter(this.props.chapterId, this.refs.edit.value);
   }
 
@@ -16,32 +14,24 @@ class ChapterBox extends Component {
   }
 
   render(){
-
-
     return (
       <div>
-        <button  onClick={()=>{this.handleEdit()}}>edit</button>
+        <button onClick={()=>{this.handleDelete()}}>delete</button>
         <textarea id="edit" ref="edit"  >{this.props.chapterData}</textarea>
-        <button onClick={()=>{this.handleDelete()}} >X</button>
+        <button  onClick={()=>{this.handleEdit()}}>edit</button>
         <li>{this.props.chapterData}</li>
       </div>
     )
   }
 }
 
-ChapterBox.propTypes = {
-  chapterData: React.PropTypes.string.isRequired,
-  chapterId: React.PropTypes.string.isRequired,
-  deleteChapter: React.PropTypes.func.isRequired,
-  editChapter: React.PropTypes.func.isRequired
+  ChapterBox.propTypes = {
+    chapterData: React.PropTypes.string.isRequired,
+    chapterId: React.PropTypes.string.isRequired,
+    deleteChapter: React.PropTypes.func.isRequired,
+    editChapter: React.PropTypes.func.isRequired
 }
 
 
-export default ChapterBox;
+  export default ChapterBox;
 
-/*<div>
-                <button  onClick={()=> {axios.put(`https://project2-a12a5.firebaseio.com/story/${chapterId}/title/.json`, [this.refs.edit.value])}}>edit</button>
-                <input  ref="edit" placeholder={chapterData.title} />
-                <li key={i}>{chapterData.title}</li>
-                <button onClick={()=>{axios.delete(`https://project2-a12a5.firebaseio.com/story/${chapterId}/.json`)}}>X</button>
-            </div>*/
